ecae713bba6cb261a7125e2d02a2149157ea23ba
Eduardo Felizardo <edufelizardo1@gmail.com>
2021-10-15 21:18:25 -0300

    Create ClienteRepository.java

---
+++ lab-padroes-projeto-java-Spring/src/main/java/com/edu/repository/ClienteRepository.java
@@ -0,0 +1,11 @@
+package com.edu.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.edu.model.Cliente;
+
+@Repository
+public interface ClienteRepository extends CrudRepository<Cliente, Long> {
+
+}
