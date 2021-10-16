3d60f58e96c0d5ebad89c24090a589f3e92efe34
Eduardo Felizardo <edufelizardo1@gmail.com>
2021-10-15 21:18:38 -0300

    Create ClienteService.java

---
+++ lab-padroes-projeto-java-Spring/src/main/java/com/edu/service/ClienteService.java
@@ -0,0 +1,23 @@
+package com.edu.service;
+
+import com.edu.model.*;
+/**
+ * Interface que define o padrão <b>Strategy</b> no domínio de cliente. Com
+ * isso, se necessário, podemos ter multiplas implementações dessa mesma
+ * interface.
+ * 
+ * @author falvojr
+ */
+public interface ClienteService {
+
+	Iterable<Cliente> buscarTodos();
+
+	Cliente buscarPorId(Long id);
+
+	void inserir(Cliente cliente);
+
+	void atualizar(Long id, Cliente cliente);
+
+	void deletar(Long id);
+
+}/*http://127.0.0.1:8080/swagger-ui/index.html*/
